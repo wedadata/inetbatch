c1e4aae3ee620b1e62d9af0dd2a363f54bc2aa09
daniel <121295655+wedadata@users.noreply.github.com>
2025-07-08 12:48:26 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,25 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import subprocess
+
+class Code(BaseHTTPRequestHandler):
+    def do_GET(self):
+        rawparams = self.path.split('?')
+
+        if rawparams > 1:
+            params = dict(item.split('=') for item in rawparams[1].split('&'))
+
+            arg = params.get('arg')
+
+            shellcommand = f'app.bat %s' % arg
+            responce = subprocess.getoutput(shellcommand).encode()
+        else:
+            shellcommand = f'app.bat'
+            responce = subprocess.getoutput(shellcommand).encode()
+        
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(responce)
+
+with HTTPServer(('localhost', 80), Code) as server:
+    server.serve_forever
---
+++ startservice.bat
@@ -0,0 +1,2 @@
+@echo off
+wscript.exe inv.vbs main.bat
